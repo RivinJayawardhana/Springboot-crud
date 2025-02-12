e9b88e7ad5fef3963feee651867b22e3ef3efe5a
Rivin Jayawardana <127378095+IT22061652@users.noreply.github.com>
2025-02-12 22:43:14 +0530

    model

--- src/main/java/com/example/Springboot/crud/repository/UserRepository.java
+++ src/main/java/com/example/Springboot/crud/repository/UserRepository.java
@@ -1,2 +1,8 @@
-package com.example.Springboot.crud.repository;public class UserRepository {
+package com.example.Springboot.crud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.Springboot.crud.model.User;
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
 }
--- src/main/java/com/example/Springboot/crud/services/UserService.java
+++ src/main/java/com/example/Springboot/crud/services/UserService.java
@@ -1,2 +1,13 @@
-package com.example.Springboot.crud.services;public class UserService {
+package com.example.Springboot.crud.services;
+
+import java.util.List;
+import java.util.Optional;
+import com.example.Springboot.crud.model.User
+
+public interface UserService {
+    List<User> getAllUsers();
+    Optional<User> getUserById(Long id);
+    User saveUser(User user);
+    User updateUser(Long id, User user);
+    void deleteUser(Long id);
 }
